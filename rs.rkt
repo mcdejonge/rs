715177d479da8510bfdbff94eda2fbd60607d059
Matthijs de Jonge <rhmtts@gmail.com>
2020-04-26 19:01:53 +0200

    Added queuing and stopping tracks to main loop

--- rs.rkt
+++ rs.rkt
@@ -2,6 +2,10 @@
 
 ;; Main rs file. Contains the main loop and functions for interacting with it.
 
+;; This should make the GC work more often, but do less work every
+;; time making performance more constant (hopefully).
+(collect-garbage 'incremental)
+
 (provide
  rs-set-global-bpm!
  rs-set-global-div-length!
@@ -9,7 +13,9 @@
  rs-start-main-loop!
  rs-stop-main-loop!)
 
-(require "rs-util.rkt")
+(require "rs-util.rkt"
+         "rs-t.rkt"
+         ffi/unsafe/atomic)
 
 (define rs-main-loop '())
 
@@ -19,26 +25,31 @@
 
 (define rs-main-div-length 1/4)
 
-(define rs-tracks-running '())
+; A list of track threads.
+(define rs-main-tracks-running '())
 
-(define rs-tracks-queued '()) ; These tracks will start at the next loop start.
+; These tracks will start at the next loop start.
+; Contains a list of rs-t structs.
+(define rs-main-tracks-queued '())
 
-(define rs-tracks-stopping '()) ; These tracks will stop at the next loop start
+; These tracks will stop at the next loop start.
+; Contains a list of indexes
+(define rs-main-tracks-stopping '())
 
-; Helper timevalues for use in sleep
+; Helper time values for use in sleep
 
 (define rs-main-div-length-ms 0.0)
 
 (define rs-main-div-time-in-secs 0.0)
 
-(define rs-main-sleep-time-in-secs 0.0) ; Not sure if this is needed?
+(define rs-main-loop-time-in-secs 0.0)
 
 (define (rs-main-recalculate-time-values!)
   (set! rs-main-div-length-ms
         (rs-calculate-div-length-ms rs-main-bpm rs-main-div-length))
   (set! rs-main-div-time-in-secs
         (/ rs-main-div-length-ms 1000))
-  (set! rs-main-sleep-time-in-secs
+  (set! rs-main-loop-time-in-secs
     (* rs-main-num-divs rs-main-div-time-in-secs)))
 
 ;; TODO contracts may seem nice, but they cause the program to stop,
@@ -63,6 +74,15 @@
   (set! rs-main-div-length div-length)
   (rs-main-recalculate-time-values!))
 
+(define/contract (rs-stop-track track-no)
+  (-> natural? void)
+  ; Add the given index to the list of indexes to stop
+  ; at the next main loop iteration start.
+  (when (< track-no (length rs-main-tracks-running))
+    (when (not (member track-no rs-main-tracks-running))
+      (cons track-no rs-main-tracks-stopping))))
+
+
 
 ; Void
 (define (rs-start-main-loop!)
@@ -72,17 +92,28 @@
         (thread
          (lambda ()
            (let loop ()
-             ; Below is an example of a loop with subdivs. The
-             ; main loop will not do this, but the individual tracks
-             ; will
-             ; TODO so how this will work is that every loop start, we check if any tracks need to be started. If so, start them.
-             ; We also stop tracks that need to be stopped.
-             (printf "Loop starts\n")
-             ; (sleep sleep-time-in-secs)
-             (for ([step-no (in-range rs-main-num-divs)])
-               (printf "Step\n")
-               (sleep rs-main-div-time-in-secs)
-               )
+
+             ; Remove tracks that need to be stopped.
+             (start-atomic)
+             (for ([track-index rs-main-tracks-stopping])
+               (let ((track-to-stop (list-ref rs-main-tracks-running track-index)))
+                 (remove track-to-stop rs-main-tracks-running)
+                 (thread-send track-to-stop 'stop)))
+             (set! rs-main-tracks-stopping '())
+             (end-atomic)
+
+             ; Start new tracks as needed.  This is atomic because the
+             ; new track queue needs to be empty when this is done. (I
+             ; think).
+             (start-atomic)
+             (for ([track rs-main-tracks-queued])
+               (let ((track-thread (rs-t-play! track)))
+                 (append rs-main-tracks-running (list track-thread))))
+             (set! rs-main-tracks-queued '())
+             (end-atomic)
+
+             (sleep rs-main-loop-time-in-secs)
+             (displayln "Looping")
              (loop))
            (printf "rs Main loop ends\n")))))
 
