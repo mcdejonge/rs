dc82aea4cf42c0239cd817797956a738a854100e
Matthijs de Jonge <rhmtts@gmail.com>
2020-04-26 16:48:59 +0200

    created rs-e.rkt : a struct for events and a method for creating them

---
+++ rs-e.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+;; Code for dealing with events.
+
+(require "rs-util.rkt")
+
+(provide rs-e
+         rs-e-create)
+
+(struct rs-e (fn offset) #:mutable #:transparent) ; TODO lookup if transparency has drawbacks.
+
+; There is probably another way to check if something is null of a procedure in a contract but I couldn't (quickly) figure out how. So use a helper function.
+(define (procedure-or-null? input)
+  (or (procedure? input) (null? input)))
+
+; Make sure no nonsensical ranges can be supplied.
+(define (offset-valid? input)
+  (and (real? input)
+       (> input -1)
+       (< input 1)))
+
+(define/contract (rs-e-create #:fn fn #:offset [offset 0])
+  ; Create an event struct with a function to run (or null) and an
+  ; optional offset (between -1 and +1)
+  (->* (#:fn procedure-or-null?
+        #:offset offset-valid?)
+       rs-e?)
+  (rs-e fn offset))
