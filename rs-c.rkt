b8452938229229504f4132fdd66d6d6e30d21dbc
Matthijs de Jonge <rhmtts@gmail.com>
2020-04-26 10:33:49 +0200

    Created mechanism for playing sequences

--- rs-c.rkt
+++ rs-c.rkt
@@ -6,54 +6,73 @@
 
 (provide rs-c
          rs-c-create
-         rs-c-play-single-loop)
+         rs-c-play
+         rs-c-play-seq)
+
 
 (struct rs-c (bpm
-              num-divisions
-              division-length
-              sequence) #:transparent) ; TODO lookup if transparency has drawbacks.
+              num-divs
+              div-length
+              seq)
+  #:mutable #:transparent) ; TODO lookup if transparency has drawbacks.
 
 ; Int, int, positive -> rs-c
 (define (rs-c-create #:bpm bpm
-                     #:num-divisions [num-divisions 16]
-                     #:division-length [division-length 1/4]
-                     #:sequence [sequence '()])
+                     #:num-divs [num-divs 16]
+                     #:div-length [div-length 1/4]
+                     #:seq [seq '()])
   ; TODO this contract does not seem to be working. Might be because
   ; we're not testing across module boundaries.
   (->* (#:bpm positive?)
-      (#:num-divisions positive?
-       #:division-length positive?
-       #:sequence list?)
+      (#:num-divs positive?
+       #:div-length positive?
+       #:seq list?)
       any)
-  (rs-c bpm num-divisions division-length sequence))
+  (rs-c bpm num-divs div-length seq))
 
 (define (rs-c-get-loop-length-ms channel)
   ; Return the channel loop length in ms)
   (-> rs-c? real?)
-  (* (rs-calculate-division-length-ms (rs-c-bpm channel)
-                                      (rs-c-division-length channel))
-     (rs-c-num-divisions channel)))
+  (* (rs-calculate-div-length-ms (rs-c-bpm channel)
+                                      (rs-c-div-length channel))
+     (rs-c-num-divs channel)))
 
 (define (rs-c-play-single-loop channel)
-  ; Play a single iteration of the current sequence for the channel.
+  ; Play a single iteration of the current seq for the channel.
   ; TODO all these calculations should be optimized so they're only done
   ; when the channel changes.
   (-> rs-c? void)
-  (let* ((loop-length-ms (rs-c-get-loop-length-ms channel))
-         (seq-item-length-ms (/ loop-length-ms (length (rs-c-sequence channel))))
-         (seq-item-length-s (/ seq-item-length-ms 1000)))
-    
-    (for ([seq-item (rs-c-sequence channel)])
-      (when (procedure? seq-item) (seq-item))
-      (sleep seq-item-length-s)
-      )
-    ))
+  (rs-c-play-seq #:length-in-ms (rs-c-get-loop-length-ms channel) #:seq (rs-c-seq channel)))
 
-;(define (rs-c-play channel)
-;  (-> rs-c? thread?)
-  ; Return a thread that plays continuously.
-  ; TODO this does not allow changing the channel values.
-;  (thread (lambda)
-;          (let loop ())
-;          )
-;  )
+(define (rs-c-play-seq #:length-in-ms length-in-ms #:seq seq)
+  (-> positive? list?)
+  ; Space the items in the seq evenly among the available time and call them.
+  (let ((seq-item-length-s (/ (/ length-in-ms (length seq)) 1000)))
+    (for ([seq-item seq])
+      (when (procedure? seq-item) (thread seq-item))
+      (sleep seq-item-length-s))))
+
+(define (rs-c-test)
+  (let* ([event
+          (lambda()
+                  (printf "Event is called\n"))]
+         [channel
+          (rs-c-create #:bpm 128 #:seq (list '() event '() event event '()))])
+    (rs-c-play channel)))
+
+
+(define (rs-c-play channel)
+  (-> rs-c? thread?)
+  ; Return a thread that plays continuously until it receives a 'stop message.
+  (thread
+   (lambda ()
+     (let loop()
+       (rs-c-play-single-loop channel)
+       (match (thread-try-receive)
+         [(? rs-c? new-channel-info)
+          (set! channel new-channel-info)
+          (loop)]
+         [ 'stop
+           (void)]
+         [ #f (loop)])))))
+      
