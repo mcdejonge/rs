54a07f90cf306ad4b08b56118769b0fc00df439e
Matthijs de Jonge <rhmtts@gmail.com>
2020-04-28 12:09:11 +0200

    added method for calling multiple event procedures simultaneously

--- rs-e.rkt
+++ rs-e.rkt
@@ -13,10 +13,18 @@
 ; procedure in a contract but I couldn't (quickly) figure out how. So
 ; use a helper function.
 (define (procedure-or-null? input)
+  ; Check if something is a procedure or null.
   (or (procedure? input) (null? input)))
 
-; Make sure no nonsensical ranges can be supplied.
+(define (list-of-event-procs? input)
+  ; Check if something is a list of procedures.
+  (and (list? input)
+       (and (procedure? (car input)) (= 1 (procedure-arity (car input))))
+       (or (null? (cdr input))
+           (list-of-event-procs? (cdr input)))))
+
 (define (offset-valid? input)
+  ; Make sure no nonsensical ranges can be supplied.
   (and (real? input)
        (> input -1)
        (< input 1)))
@@ -28,3 +36,25 @@
         (#:offset offset-valid?)
        rs-e?)
   (rs-e fn offset))
+
+(define/contract (rs-e-multiple procedures)
+  ; Return a procedure that runs all the supplied procedures.
+  (-> list-of-event-procs? procedure?)
+  (lambda (step-time)
+    (for ([procedure procedures])
+      (thread (lambda ()
+                (procedure step-time) )))))
+
+(module+ test
+  (define (rs-e-test)
+    (let* ([proc1
+            (lambda (step-time)
+              (printf "Calling proc 1 with argument ~a\n" step-time))]
+           [proc2
+            (lambda (step-time)
+              (printf "Calling proc 2 with argument ~a\n" step-time))]
+           [multi
+            (rs-e-multiple (list proc1 proc2))])
+      (printf "You should see both proc1 and proc2 being called with argument 3.\n")
+      (multi 3)))
+  (rs-e-test))
