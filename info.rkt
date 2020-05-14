fd016420a76078261f6187866fcfd8fdb9caade5
Matthijs de Jonge <matthijs@rommelhok.com>
2020-05-14 22:39:30 +0200

    Scribble doc was accidentally split into multiple files. Undid that.

--- info.rkt
+++ info.rkt
@@ -3,5 +3,5 @@
 (define collection "rs")
 (define deps '("base"
                "rtmidi"))
-(define scribblings '(("scribblings/rs.scrbl" (multi-page))))
+(define scribblings '(("scribblings/rs.scrbl" )))
 (define build-deps '("scribble-lib" "racket-doc"))
