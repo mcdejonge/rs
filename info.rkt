89af68f3bf8f9b093d54eefa03973c20da6ca5be
Matthijs de Jonge <matthijs@rommelhok.com>
2020-05-15 08:56:01 +0200

    added missing dependency

--- info.rkt
+++ info.rkt
@@ -2,6 +2,7 @@
 #lang info
 (define collection "rs")
 (define deps '("base"
+               "rackunit"
                "rtmidi"))
 (define scribblings '(("scribblings/rs.scrbl" )))
 (define build-deps '("scribble-lib" "racket-doc"))
